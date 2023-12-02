385e4798ae99e6f276e759c68158232ace5756ac
bruno <bruno.resende@gmx.com>
2023-12-02 13:59:52 -0300

    Fix: Layout Shift

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
-import { clsx } from "clsx";
-import { useState } from "react";
-import { Form } from "./components/Form";
-import { Nav } from "./components/Nav";
+import { clsx } from 'clsx';
+import { useState } from 'react';
+import { Form } from './components/Form';
+import { Nav } from './components/Nav';
 
 export interface Data {
-  sexo: "Masculino" | "Feminino";
-  corrida: { value: number; score: number };
-  flexao: { value: number; score: number };
-  abdominal: { value: number; score: number };
-  natacao: { value: number; score: number };
-  barras: { value: number; score: number };
+    sexo: 'Masculino' | 'Feminino';
+    corrida: { value: number; score: number };
+    flexao: { value: number; score: number };
+    abdominal: { value: number; score: number };
+    natacao: { value: number; score: number };
+    barras: { value: number; score: number };
 }
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
-  const [data, setData] = useState<Data>({
-    sexo: "Masculino",
-    corrida: { value: 0, score: 0 },
-    flexao: { value: 0, score: 0 },
-    abdominal: { value: 0, score: 0 },
-    natacao: { value: 0, score: 0 },
-    barras: { value: 0, score: 0 },
-  });
+    const [darkMode, setDarkMode] = useState(true);
+    const [data, setData] = useState<Data>({
+        sexo: 'Masculino',
+        corrida: { value: 0, score: 0 },
+        flexao: { value: 0, score: 0 },
+        abdominal: { value: 0, score: 0 },
+        natacao: { value: 0, score: 0 },
+        barras: { value: 0, score: 0 },
+    });
 
-  return (
-    <div className={clsx(darkMode && "dark text-white")}>
-      <Nav dark={darkMode} setDark={setDarkMode} />
-      <Form {...{ data, setData }} />
-    </div>
-  );
+    return (
+        <div
+            className={clsx(
+                'h-dvh flex flex-col',
+                darkMode && 'dark text-white'
+            )}
+        >
+            <Nav dark={darkMode} setDark={setDarkMode} />
+            <Form {...{ data, setData }} />
+            <footer className="text-gray-500 dark:border-gray-800 mt-auto py-5 flex">
+                <span className="mx-auto">
+                    ©Devisio {new Date().getFullYear()}
+                </span>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
